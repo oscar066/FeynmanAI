4f1319454c81a91ff031aefbc0b93a90c81f0609
oscar066 <oscarkaruga1@gmail.com>
2024-08-08 17:41:16 +0300

    :construction: Docs Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="FeynmanAi",
-    version="0.1.0",
+    version="0.1.2",
     description="A command-line tool for using RAG model on Local documents",
     long_description=long_description,
     long_description_content_type="text/markdown",
